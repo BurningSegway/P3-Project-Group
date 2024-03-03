507cdda9ccaee1992ca068be23c73bf5fdf01b1e
BurningSegway <pmorte22@student.aau.dk>
2024-03-03 22:52:49 +0100

    Oprydning

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pe/Desktop/P3-Project-Group/catkin_ws/src
-# Build directory: /home/pe/Desktop/P3-Project-Group/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/pe/Desktop/P3-Project-Group/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/pe/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/pe/catkin_ws/devel;/opt/ros/noetic' != '' else []
